e0c719134f881661147cc70348e9afd56a0054f9
Sumin <new.sumyang@gmail.com>
2025-10-28 22:14:44 +0900

    feat(AppConfig): 입력 클래스 설정 추가

--- src/main/java/lotto/application/config/AppConfig.java
+++ src/main/java/lotto/application/config/AppConfig.java
@@ -1,11 +1,17 @@
 package lotto.application.config;
 
 import lotto.application.Printer;
+import lotto.application.Reader;
 import lotto.application.lottoprinter.LottoPrinter;
+import lotto.application.lottoreader.LottoReader;
 
 public class AppConfig {
 
     public Printer printer() {
         return new LottoPrinter();
+    }
+
+    public Reader reader() {
+        return new LottoReader();
     }
 }
