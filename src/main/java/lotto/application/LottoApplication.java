7e072ef06ee4282ab538189e7bdb4ca34426fc3f
Sumin <new.sumyang@gmail.com>
2025-10-28 22:44:54 +0900

    feat(LottoApplication): 구입 금액 입력 로직 추가

--- src/main/java/lotto/application/LottoApplication.java
+++ src/main/java/lotto/application/LottoApplication.java
@@ -1,15 +1,19 @@
 package lotto.application;
 
 import lotto.application.config.AppConfig;
+import lotto.domain.PurchasePrice;
 
 public class LottoApplication {
     private final Printer printer;
+    private final Reader reader;
 
     public LottoApplication(AppConfig appConfig) {
-        this.printer = appConfig.printer();
+        printer = appConfig.printer();
+        reader = appConfig.reader();
     }
 
     public void run() {
         printer.printPurchaseAmountRequest();
+        PurchasePrice purchasePrice = PurchasePrice.from(reader.read());
     }
 }
