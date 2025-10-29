e2bb7df2eaf2b5ad0607cf518ae58be1772e81b3
Sumin <new.sumyang@gmail.com>
2025-10-30 02:24:11 +0900

    feat(Lotto): 정팩메 도입

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,8 +1,12 @@
 package lotto.domain;
 
 import java.util.List;
+import lotto.exception.LottoErrorCode;
+import lotto.exception.LottoException;
 
 public class Lotto {
+
+    private static final int NUMBER_SIZE = 6;
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -10,10 +14,18 @@ public class Lotto {
         this.numbers = numbers;
     }
 
+    public static Lotto from(List<Integer> numbers) {
+        return new Lotto(numbers);
+    }
+
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+        if (numbers.size() != NUMBER_SIZE) {
+            throw new LottoException(LottoErrorCode.INVALID_LOTTO_NUMBER_COUNT, NUMBER_SIZE);
         }
+    }
+
+    public List<Integer> getNumbers() {
+        return numbers;
     }
 
     // TODO: 추가 기능 구현
