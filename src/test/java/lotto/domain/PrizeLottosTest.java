0232c9e6698b1314309598dc1aa2605b112ec8bb
Sumin <new.sumyang@gmail.com>
2025-11-01 02:55:42 +0900

    test(PrizeLottosTest): 로또 rank에 따른 정렬 기능 확인

---
+++ src/test/java/lotto/domain/PrizeLottosTest.java
@@ -0,0 +1,44 @@
+package lotto.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.ArrayList;
+import java.util.List;
+import lotto.domain.prizelotto.FifthPrize;
+import lotto.domain.prizelotto.FirstPrize;
+import lotto.domain.prizelotto.FourthPrize;
+import lotto.domain.prizelotto.PrizeLotto;
+import lotto.domain.prizelotto.SecondPrize;
+import lotto.domain.prizelotto.ThirdPrize;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class PrizeLottosTest {
+
+    @Test
+    @DisplayName("rank값을 기준으로 내림차순 정렬을 확인한다.")
+    void sortingLottoByRank() {
+        // given
+        PrizeLotto firstPrize = new FirstPrize();
+        PrizeLotto secondPrize = new SecondPrize();
+        PrizeLotto thirdPrize = new ThirdPrize();
+        PrizeLotto fourthPrize = new FourthPrize();
+        PrizeLotto fifthPrize = new FifthPrize();
+        List<PrizeLotto> lottos = List.of(firstPrize, secondPrize, thirdPrize, fourthPrize, fifthPrize);
+        PrizeLottos prizeLottos = PrizeLottos.from(new ArrayList<>(lottos));
+
+        // when
+        prizeLottos.sortByRank();
+
+        // then
+        assertThat(prizeLottos.getValue())
+                .containsExactly(
+                        fifthPrize,
+                        fourthPrize,
+                        thirdPrize,
+                        secondPrize,
+                        firstPrize
+                );
+    }
+
+}
