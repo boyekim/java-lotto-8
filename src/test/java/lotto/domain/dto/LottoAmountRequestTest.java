fbdb06dd99fa9da21ae18cd845afe0131c5edaed
Sumin <new.sumyang@gmail.com>
2025-11-01 20:58:24 +0900

    test(LottoAmountRequestTest): 로또 구매 정보를 담은 dto 테스트 작성

---
+++ src/test/java/lotto/domain/dto/LottoAmountRequestTest.java
@@ -0,0 +1,30 @@
+package lotto.domain.dto;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import lotto.domain.LottoAmount;
+import lotto.domain.PurchasePrice;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class LottoAmountRequestTest {
+
+    @Test
+    @DisplayName("로또 구매 정보를 담은 dto 생성을 확인한다.")
+    void lottoAmountRequest() {
+        // given
+        PurchasePrice purchasePrice = PurchasePrice.from("5000");
+        LottoAmount lottoAmount = LottoAmount.from(5000);
+
+        // when
+        LottoAmountRequest lottoAmountRequest = LottoAmountRequest.of(lottoAmount, purchasePrice);
+
+        // then
+        assertThat(lottoAmountRequest)
+                .extracting(
+                        request -> lottoAmount.getValue(),
+                        request -> purchasePrice.getValue())
+                .isEqualTo(List.of(5L, 5000L));
+    }
+}
