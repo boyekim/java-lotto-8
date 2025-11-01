012581e1a3b7957c56b0075ea0f6af40b49f1043
Sumin <new.sumyang@gmail.com>
2025-11-01 20:28:46 +0900

    test(PrizeLottoTest): 각 당첨 조건 구현체 조건 만족 테스트 작성

---
+++ src/test/java/lotto/domain/prizelotto/PrizeLottoTest.java
@@ -0,0 +1,29 @@
+package lotto.domain.prizelotto;
+
+import java.util.stream.Stream;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class PrizeLottoTest {
+
+    @ParameterizedTest
+    @DisplayName("각 로또의 당첨 조건을 확인한다.")
+    @MethodSource("providePrizeLotto")
+    void satisfyWinningRequirement(int mainMatchCount, boolean isBonusMatch, PrizeLotto prizeLotto) {
+        boolean isSatisfyPrizeRule = prizeLotto.isSatisfyWinningRequirement(mainMatchCount, isBonusMatch);
+        Assertions.assertThat(isSatisfyPrizeRule).isTrue();
+    }
+
+    private static Stream<Arguments> providePrizeLotto() {
+        return Stream.of(
+                Arguments.of(6, false, new FirstPrize()),
+                Arguments.of(5, true, new SecondPrize()),
+                Arguments.of(5, false, new ThirdPrize()),
+                Arguments.of(4, false, new FourthPrize()),
+                Arguments.of(3, false, new FifthPrize())
+        );
+    }
+}
