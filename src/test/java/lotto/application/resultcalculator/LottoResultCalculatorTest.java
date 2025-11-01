1e187c35010a322823629495e81ec38f74b789a0
Sumin <new.sumyang@gmail.com>
2025-11-01 21:36:19 +0900

    test(LottoResultCalculatorTest): 수익률 계산 로직 테스트 작성

---
+++ src/test/java/lotto/application/resultcalculator/LottoResultCalculatorTest.java
@@ -0,0 +1,44 @@
+package lotto.application.resultcalculator;
+
+import java.util.ArrayList;
+import java.util.List;
+import lotto.domain.prizelotto.FifthPrize;
+import lotto.domain.prizelotto.FirstPrize;
+import lotto.domain.prizelotto.FourthPrize;
+import lotto.domain.prizelotto.PrizeLotto;
+import lotto.domain.prizelotto.SecondPrize;
+import lotto.domain.prizelotto.ThirdPrize;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class LottoResultCalculatorTest {
+
+    @Test
+    @DisplayName("수익률 계산을 검증한다.")
+    void calculateProfit() {
+        // given
+        LottoResultCalculator calculator = new LottoResultCalculator();
+        long purchasePrice = 8000;
+        FirstPrize firstPrize = new FirstPrize();
+        SecondPrize secondPrize = new SecondPrize();
+        ThirdPrize thirdPrize = new ThirdPrize();
+        FourthPrize fourthPrize = new FourthPrize();
+        FifthPrize fifthPrize = new FifthPrize();
+        fifthPrize.upCount();
+        List<PrizeLotto> prizeLottos = new ArrayList<>(List.of(
+                firstPrize,
+                secondPrize,
+                thirdPrize,
+                fourthPrize,
+                fifthPrize
+        ));
+
+        // when
+        double profit = calculator.calculateProfit(purchasePrice, prizeLottos);
+        String result = String.format("%.1f", profit);
+
+        // then
+        Assertions.assertThat(result).isEqualTo("62.5");
+    }
+}
